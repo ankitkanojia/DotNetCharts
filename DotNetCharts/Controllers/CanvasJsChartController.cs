2ba4c37af72c3f68a9d2b9ff36137d347953b50d
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-12-17 11:21:32 +0530

    implement Canvasjs chart in new canvasjs controller file and implement sample chart in index.cshtml file

---
+++ DotNetCharts/Controllers/CanvasJsChartController.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetCharts.Controllers
+{
+    [DataContract]
+    public class DataPoint
+    {
+        public DataPoint(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "x")]
+        public Nullable<double> X = null;
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "y")]
+        public Nullable<double> Y = null;
+    }
+
+    public class CanvasJsChartController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<DataPoint> dataPoints = new List<DataPoint>{
+                new DataPoint(10, 22),
+                new DataPoint(20, 36),
+                new DataPoint(30, 42),
+                new DataPoint(40, 51),
+                new DataPoint(50, 46),
+            };
+
+            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+
+            return View();
+        }
+    }
+}
