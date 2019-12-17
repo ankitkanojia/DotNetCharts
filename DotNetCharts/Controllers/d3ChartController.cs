93d3c8aeb774861aae9a87e3edcbc2a4bd50b340
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-12-17 11:58:15 +0530

    Integrate sample d3.js chart into the system with sample example of line chart

---
+++ DotNetCharts/Controllers/d3ChartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetCharts.Controllers
+{
+    public class d3ChartController : Controller
+    {
+        // GET: d3Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
