1e75797f5db07b9f02b1449d433d55f2763a2261
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-12-26 10:54:54 +0530

    Remove exisiting package of highchart rather used javascript library

--- DotNetCharts/Controllers/HighChartController.cs
+++ DotNetCharts/Controllers/HighChartController.cs
@@ -1,18 +1,11 @@
-﻿using DotNet.Highcharts;
-using DotNet.Highcharts.Enums;
-using DotNet.Highcharts.Helpers;
-using DotNet.Highcharts.Options;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace DotNetCharts.Controllers
 {
     public class HighChartController : Controller
     {
+        //Library Link : https://www.highcharts.com/demo
+
         public ActionResult Index()
         {
             return View();
@@ -20,72 +13,7 @@ namespace DotNetCharts.Controllers
 
         public ActionResult ColumnChart()
         {
-            Highcharts columnChart = new Highcharts("columnchart");
-
-            columnChart.InitChart(new Chart()
-            {
-                Type = DotNet.Highcharts.Enums.ChartTypes.Column,
-                BackgroundColor = new BackColorOrGradient(System.Drawing.Color.AliceBlue),
-                Style = "fontWeight: 'bold', fontSize: '17px'",
-                BorderColor = System.Drawing.Color.LightBlue,
-                BorderRadius = 0,
-                BorderWidth = 2
-
-            });
-
-            columnChart.SetTitle(new Title()
-            {
-                Text = "Sachin Vs Dhoni"
-            });
-
-            columnChart.SetSubtitle(new Subtitle()
-            {
-                Text = "Played 9 Years Together From 2004 To 2012"
-            });
-
-            columnChart.SetXAxis(new XAxis()
-            {
-                Type = AxisTypes.Category,
-                Title = new XAxisTitle() { Text = "Years", Style = "fontWeight: 'bold', fontSize: '17px'" },
-                Categories = new[] { "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012" }
-            });
-
-            columnChart.SetYAxis(new YAxis()
-            {
-                Title = new YAxisTitle()
-                {
-                    Text = "Runs",
-                    Style = "fontWeight: 'bold', fontSize: '17px'"
-                },
-                ShowFirstLabel = true,
-                ShowLastLabel = true,
-                Min = 0
-            });
-
-            columnChart.SetLegend(new Legend
-            {
-                Enabled = true,
-                BorderColor = System.Drawing.Color.CornflowerBlue,
-                BorderRadius = 6,
-                BackgroundColor = new BackColorOrGradient(ColorTranslator.FromHtml("#FFADD8E6"))
-            });
-
-            columnChart.SetSeries(new Series[]
-            {
-                new Series{
-
-                    Name = "Sachin Tendulkar",
-                    Data = new Data(new object[] { 812, 412, 628, 1425, 460, 972, 204, 513, 315 })
-                },
-                new Series()
-                {
-                    Name = "M S Dhoni",
-                    Data = new Data(new object[] { 19, 895, 821, 1103, 1097, 1198, 600, 764, 524, })
-                }
-            }
-            );
-
-            return View(columnChart);
+            return View();
         }
 
         public ActionResult BarChart()
