94814402a35db2bab61139cd0c8fca6d621ed50a
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-12-27 11:55:55 +0530

    Integrate Area chart using High chart's javascript library.

--- DotNetCharts/Controllers/HighChartController.cs
+++ DotNetCharts/Controllers/HighChartController.cs
@@ -25,5 +25,10 @@ namespace DotNetCharts.Controllers
         {
             return View();
         }
+
+        public ActionResult AreaChart()
+        {
+            return View();
+        }
     }
 }
