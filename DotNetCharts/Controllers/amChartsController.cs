495513c9e48bb13349beffadae909e6b0a29560d
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2020-01-27 09:52:43 +0530

    Implement chord diagram using amchart javascript chart library.

--- DotNetCharts/Controllers/amChartsController.cs
+++ DotNetCharts/Controllers/amChartsController.cs
@@ -24,5 +24,10 @@ namespace DotNetCharts.Controllers
         {
             return View();
         }
+
+        public ActionResult ChordDiagram()
+        {
+            return View();
+        }
     }
 }
